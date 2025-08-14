19df8b4aaa470eec8ab5eaf847a853a1c36340f1
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2025-08-14 00:30:00 -0400

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -15,6 +15,9 @@ export const AuthContextProvider = ({ children }) => {
 
     // sign in
     const signInUser = async (email, password) => {
+        if (!email.endsWith("@alumnos.ucn.cl")) {
+        return { success: false, error: "Solo se permite el correo institucional};
+        }
         try {
             const { data, error } = await supabase.auth.signInWithPassword({ email, password });
             if (error) {
