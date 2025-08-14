5343ce50b7762e9aad3e86f22fd02f74e601b44d
julian honores <julian.honores@alumnos.ucn.cl>
2025-08-14 01:15:39 -0400

    creacion de usuarios lista

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,6 +11,7 @@ const Signup = () => {
     const { session, signUpuser } = useAuth();
     const navigate = useNavigate();
     console.log(session);
+    // console.log(email, password);
 
     const handleSignup = async (e) => {
         e.preventDefault();
@@ -30,13 +31,14 @@ const Signup = () => {
 
   return (
     <div className='min-h-screen flex items-center justify-center'>
-        <form className='max-w-md w-full px-6'>
+        <form onSubmit={handleSignup} className='max-w-md w-full px-6'>
             <h2 className='font-bold pb-2 text-center text-2xl'>Signup</h2>
             <p className='text-center mb-4'>Ya tienes una cuenta? <Link to="/signin">Inicia sesión!</Link></p>
             <div className='flex flex-col py-4'>
-                <input className='p-3 mt-6 text-white' style={{backgroundColor: '#0D0D0D'}} type="email" name='email' id='signup-email' placeholder='Email'/>
-                <input className='p-3 mt-6 text-white' style={{backgroundColor: '#0D0D0D'}} type="password" name='password' id='signup-password' placeholder='Password'/>
+                <input onChange={(e) => setEmail(e.target.value)} className='p-3 mt-6 text-white' style={{backgroundColor: '#0D0D0D'}} type="email" name='email' id='signup-email' placeholder='Email'/>
+                <input onChange={(e) => setPassword(e.target.value)} className='p-3 mt-6 text-white' style={{backgroundColor: '#0D0D0D'}} type="password" name='password' id='signup-password' placeholder='Password'/>
                 <button type='submit' disabled={loading} className='mt-6 w-full'>Crear cuenta</button>
+                {error && <p className='text-red-600 text-center pt-4'>{error}</p>}
             </div>
         </form>
     </div>
