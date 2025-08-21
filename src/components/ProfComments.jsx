550d42182ee1eb6a750ce89441d313fc3d04be39
D.R. <PFACC4repo@gmail.com>
2025-08-21 15:40:26 -0400

    ahora si funciona mi cagá

--- src/components/ProfComments.jsx
+++ src/components/ProfComments.jsx
@@ -1,12 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import logo from '../assets/smallwhitelogo.png'; // Asegúrate de que la ruta al logo sea correcta
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { supabase } from '../supabaseClient';
 
 
 const ProfComments = () => {
   const { id } = useParams();
+  const [profesorUI, setProfesorUI] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [namensuch, setNamensuch] = useState('');
+
+  useEffect(() => {
+    fetchProfesorData();
+    }, [id]); // Fetch data when the component mounts or id changes
   
   const { session, signOut } = useAuth();
   const navigate = useNavigate();
@@ -15,6 +23,29 @@ const ProfComments = () => {
 
   const handleLogoClick = () => {
     navigate('/dashboard');
+  };
+
+  const fetchProfesorData = async () => {
+    setIsLoading(true);
+    try {
+      
+      const { data: profesores, error: profesoresError } = await supabase
+        .from('profesores')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+      if (profesoresError) throw profesoresError;
+
+        setNamensuch(profesores.nombre_apellido);
+
+
+    } catch (error) {
+        console.error('Error fetching shit:', error);
+    } finally {
+        setIsLoading(false);
+    }
+
   };
 
   return (
@@ -31,11 +62,17 @@ const ProfComments = () => {
                 />
             </div>
         </div>
-
-        <div>
-            <h1 className="text-2xl font-bold text-center mt-8">Comentarios del Profesor</h1>
-            <p className="text-center mt-4">Aquí puedes ver y gestionar los comentarios del profesor con ID: {id}</p>
+            
+        <div className="text-center mt-8">
+            <h1 className="text-2xl font-bold mb-4">
+            {isLoading ? 'Cargando...' : namensuch || 'Profesor no encontrado'}
+            </h1>
+            <p className="text-gray-400">
+            ID del profesor: {id}
+            </p>
         </div>
+
+        
     </div>
   );
 };
